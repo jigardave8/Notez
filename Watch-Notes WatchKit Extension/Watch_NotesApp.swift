c0f2b8a438ca9e4f90979bf89527e41071a74566
Jigar Dave <jdavenz8@gmail.com>
2021-06-14 13:36:15 +0530

    Initial Commit

---
+++ Watch-Notes WatchKit Extension/Watch_NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  Watch_NotesApp.swift
+//  Watch-Notes WatchKit Extension
+//
+//  Created by jigar dave on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Watch_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
